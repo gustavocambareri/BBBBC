e6ed0c9ae81b9753b352d1c0c50fd69370266e4f
gustavocambareri <gustavo.cambareri@gmail.com>
2023-10-27 20:31:53 +0200

    gg

--- sketch.js
+++ sketch.js
@@ -1,12 +1,23 @@
 var canvas
+let bombSound
+function preload(){
+bombSound= loadSound('sounds/bomb2.mp3')
+}
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0,0)
-  canvas.style('z-index','0')
+  canvas.style('z-index','-1')
+  setInterval(playBomb, 6500);
+  bombSound.play()
 }
 
 function draw() {
 
-circle(mouseX,mouseY,20)
+
+}
+
+function playBomb(){
+
+  bombSound.play()
 }
